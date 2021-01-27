621eacd635a4507c48bec53d9818a4e334c7edac
Diana Gouveia <dianacpgouveia@gmail.com>
2021-01-27 21:49:37 +0000

    added files

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const knex = require('knex');
+const bcrypt = require('bcrypt-nodejs');
+const Clarifai = require('clarifai');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'lorennav',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+
+
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(cors())
+
+app.get('/', (req, res)=> {res.send('it is working!')})
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
+
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
+app.listen(process.env.PORT || 4000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
+})
+
+/*
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userID --> GET = user
+/image --> PUT --> user
+
+*/
