e191cd8c67bcccb4af6232a6f36a65b66220ab6e
Diana Gouveia <dianacpgouveia@gmail.com>
2021-01-28 15:24:36 +0000

    fixed heroku database

--- server.js
+++ server.js
@@ -14,10 +14,9 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-perpendicular-63396',
-    user : 'lorennav',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true,
+
   }
 });
 
