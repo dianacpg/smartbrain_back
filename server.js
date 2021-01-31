1366deb50bc362f7134aa6ba72b158beaaf21e86
Diana Gouveia <dianacpgouveia@gmail.com>
2021-01-31 18:47:56 +0000

    took off more cors thing

--- server.js
+++ server.js
@@ -25,13 +25,6 @@ const db = knex({
 const app = express();
 
 app.use(cors())
-app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
-
 
 app.use(bodyParser.json());
 
